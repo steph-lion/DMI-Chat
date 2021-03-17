a0e2db9a7c9037947474ad4c2d2cdbe267e985a1
steph-lion <stefano.leone99@gmail.com>
2021-03-17 19:00:22 +0100

    Update

--- public/script/chat.js
+++ public/script/chat.js
@@ -1,5 +1,9 @@
 var socket = io.connect('http://localhost:3000');
 $("#join").on("click",function () {
-  $("#join").fadeOut("500");
+  $("#join").fadeOut("500", function(){
+    $("#div_chat_list").slideDown("slow").fadeIn("slow");
+  });
+  
+  
   socket.emit('message','Avaia');
 });
