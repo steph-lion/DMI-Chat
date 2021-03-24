f939022b2b4f0303a92883fc857c3ec5e5018c6d
steph-lion <stefano.leone99@gmail.com>
2021-03-24 14:44:59 +0100

    Update

--- public/script/chat.js
+++ public/script/chat.js
@@ -1,1 +1,7 @@
 //var socket = io.connect('http://localhost:3000');
+function fillRooms(){
+   var i;
+   for (i=1;i<6;i++){
+    $("#box_containing_rooms").append("<button class='room-buttons'>Room "+i+"</button><br>");
+   }
+}
